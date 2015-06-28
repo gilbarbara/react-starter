a9efe48c14fdfc307f20d42dd4bd92096b311bd2
Gil Barbara <gilbarbara@gmail.com>
2015-06-28 16:17:22 -0300

    Update 2015-06-28T19:17:20.504Z

--- app/scripts/config.js
+++
@@ -1,8 +0,0 @@
-var NPMPackage = require('../../package.json');
-module.exports = {
-	items: [
-		{ react: NPMPackage.dependencies.react.replace(/~|\^/, '') },
-		{ 'react-router': NPMPackage.dependencies['react-router'].replace(/~|\^/, '') },
-		{ 'react-bootstrap': NPMPackage.dependencies['react-bootstrap'].replace(/~|\^/, '') }
-	]
-};
